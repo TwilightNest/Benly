d274f9561400138545397bbb33f7b49c2e276375
TwilightNest <Jarik_Voinski@mail.ru>
2022-09-08 17:44:35 +0300

    Initial Commit

---
+++ Benly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Benly
+//
+//  Created by twilightnest on 8.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
